770e8b0c97ac7ab4645678d7dfaf757491c08d6e
Hadassa-gal <hadassa.rgr@gmail.com>
2026-03-26 08:06:02 -0500

    .

--- Practica-1-cecar/pandas_1.py
+++ Practica-1-cecar/pandas_1.py
@@ -1,5 +1,12 @@
 import pandas as pd
 from datadata import estudiantes
 df = pd.DataFrame(estudiantes)  
-print(df)
+# print(df)
+# df.info()
+# print(df.isna().sum())
+# print(df.isna().sum().sum())
+# print(df.isna().mean()*100)
+# print(df[df.isna().all(axis=1)])
+df= df.dropna(how='all')
 df.info()
+print(df[df.isna().all(axis=1)])
